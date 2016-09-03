2741448e7fffaea5f68a0f76ef3227a83331c97b
Charles Milam <charles.milam@gmail.com>
2016-09-03 03:01:27 -0500

    concordance exercises from nltk chapter 1

--- chap1/1_1.py
+++ chap1/1_1.py
@@ -1,3 +1,14 @@
 from nltk.book import *
 
+# print title of text6
 print(text6)
+
+# print concordance of 'monstrous' from Moby Dick
+print(text1.concordance('monstrous'))
+
+# explore other concordances in different texts
+print(text6.concordance('rabbit'))
+print('====================================================')
+print(text3.concordance('lived'))
+print('++++++++++++++++++++++++++++++++++++++++++++++++++++')
+print(text5.concordance('fuck'))

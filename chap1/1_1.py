9d0f5b1a37b674c22612d74a19e88711e892174b
Charles Milam <charles.milam@gmail.com>
2016-09-03 09:18:00 -0500

    exercises in similarities and common context. add variables to hold oft used line breaks.

--- chap1/1_1.py
+++ chap1/1_1.py
@@ -1,5 +1,8 @@
 from nltk.book import *
 
+# helper line breaks
+lb1 = '====================================================='
+lb2 = '+++++++++++++++++++++++++++++++++++++++++++++++++++++'
 # print title of text6
 print(text6)
 
@@ -8,7 +11,24 @@ print(text1.concordance('monstrous'))
 
 # explore other concordances in different texts
 print(text6.concordance('rabbit'))
-print('====================================================')
+print(lb1)
 print(text3.concordance('lived'))
-print('++++++++++++++++++++++++++++++++++++++++++++++++++++')
+print(lb2)
 print(text5.concordance('fuck'))
+
+# print similarities to monstrous in Moby Dick and S & S
+print(text1.similar('monstrous'))
+print(lb1)
+print(text2.similar('monstrous'))
+print(lb2)
+
+# print common contexts for very and monstrous in S & S
+print(text2.common_contexts(['monstrous', 'very']))
+print(lb1)
+
+# explore similar and common_contexts
+print(text6.similar('king'))
+print(lb2)
+print(text3.similar('king'))
+print(lb1)
+print(text5.common_contexts(['love', 'fuck']))

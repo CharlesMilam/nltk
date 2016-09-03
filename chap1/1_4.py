2063f7ea78151c2c5116de866575c68aef4792b6
Charles Milam <charles.milam@gmail.com>
2016-09-03 13:09:56 -0500

    add exploration of # of occurrences and % for some text.

--- chap1/1_4.py
+++ chap1/1_4.py
@@ -1,3 +1,12 @@
 import numpy
 import matplotlib
 from nltk.book import *
+
+# get length of text3 - book of Genesis
+print('Genesis has ' + str(len(text3)) + ' words.')
+
+# find number of occurrences and percentage for 'lol'
+lol_cnt = float(text5.count('lol'))
+chat_len = float(len(text5))
+print(lol_cnt)
+print(100 * (lol_cnt / chat_len))

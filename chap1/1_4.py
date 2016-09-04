e748fcca23f2cd5a8e71477e18d451b638308cd2
Charles Milam <charles.milam@gmail.com>
2016-09-03 22:04:29 -0500

    add functions for determining lexical diverstiy and percentage

--- chap1/1_4.py
+++ chap1/1_4.py
@@ -10,3 +10,19 @@ lol_cnt = float(text5.count('lol'))
 chat_len = float(len(text5))
 print(lol_cnt)
 print(100 * (lol_cnt / chat_len))
+
+# create function to determine lexical diversity
+def lex_div(text):
+    return 100 * (float(len(set(text))) / float(len(text)))
+
+#create function to determine %
+def percentage(count, total):
+    return 100 * (float(count) / float(total))
+
+# print lexical diversity of Genesis
+print(lex_div(text3))
+
+# print %
+count = text4.count('a')
+total = len(text4)
+print(percentage(count, total))

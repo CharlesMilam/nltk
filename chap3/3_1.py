071be2aa58f626566b57c659f657485110a66a42
Charles Milam <charles.milam@gmail.com>
2016-09-04 00:24:25 -0500

    explore word distributions, to include frequency distribution, most common, hapaxes, and collocations.

--- chap3/3_1.py
+++ chap3/3_1.py
@@ -1,3 +1,31 @@
 import numpy
 import matplotlib
 from nltk.book import *
+
+# line break
+lb = '+++++++++++++++++++++++++++++++++++++++++++++++++++++'
+
+# get frequency distribution of Monty Python
+fdist1 = FreqDist(text6)
+print(fdist1)
+print(lb)
+
+# get the 50 most common tokens
+print(fdist1.most_common(50))
+print(lb)
+
+# get the hapaxes
+print(fdist1.hapaxes())
+print(lb)
+
+# plot fdist1
+# fdist1.plot(50, cumulative = True)
+
+# get long words
+uniqs = set(text6)
+long_words = [word for word in uniqs if len(word) > 10]
+print(sorted(long_words))
+print(lb)
+
+# get collocations
+print(text6.collocations())
